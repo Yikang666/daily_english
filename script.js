70ee9e61a3231071a50bcd9364729bd3823478a8
Yikang666 <2749436915@qq.com>
2024-09-21 22:42:02 +0800

    update: 首次提交

---
+++ script.js
@@ -0,0 +1,18 @@
+// 发送请求
+const xhr = new XMLHttpRequest();
+
+function sendGetRequest(url, callback) {
+  xhr.open("GET", url);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      callback(xhr.response);
+    }
+  };
+  xhr.send();
+}
+
+sendGetRequest("http://mirror.666-114514.eu.org/https://open.iciba.com/dsapi/", function (response) {
+  const data = JSON.parse(response);
+  document.querySelector('.banner img').setAttribute('src', data.fenxiang_img);
+  console.log(data);
+});
